76c7c4055289d69d1bf6fb733138067f4851d6d4
Mohammad Awad <112518004+mohammaw@users.noreply.github.com>
2023-11-12 21:51:30 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -5,7 +5,74 @@ window.addEventListener("load", function() {
 
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+var video;
+
+window.addEventListener("load", function() {
+    console.log("Good job opening the window");
+    video = document.querySelector('video');
+    video.autoplay = false;
+    video.loop = false;
+});
+
+// Play Button
+document.querySelector("#play").addEventListener("click", function() {
+    console.log("Play Video");
+    video.play();
+    updateVolumeInfo();
+});
+
+// Pause Button
+document.querySelector("#pause").addEventListener("click", function() {
+    console.log("Pause Video");
+    video.pause();
+});
+
+// Slow Down
+document.querySelector("#slower").addEventListener("click", function() {
+    video.playbackRate *= 0.9;
+    console.log("New speed is " + video.playbackRate);
+});
+
+// Speed Up
+document.querySelector("#faster").addEventListener("click", function() {
+    video.playbackRate /= 0.9;
+    console.log("New speed is " + video.playbackRate);
+});
+
+// Skip Ahead
+document.querySelector("#skip").addEventListener("click", function() {
+    if (video.currentTime + 10 > video.duration) {
+        video.currentTime = 0;
+    } else {
+        video.currentTime += 10;
+    }
+    console.log("Current location is " + video.currentTime);
+});
+
+// Mute
+document.querySelector("#mute").addEventListener("click", function() {
+    if (video.muted) {
+        video.muted = false;
+        this.textContent = "Mute";
+    } else {
+        video.muted = true;
+        this.textContent = "Unmute";
+    }
+});
+
+// Volume Slider
+document.querySelector("#volumeSlider").addEventListener("input", function() {
+    video.volume = this.value / 100;
+    updateVolumeInfo();
+});
+
+// Update Volume Info
+function updateVolumeInfo() {
+    console.log("Volume level is " + video.volume * 100 + "%");
+}
+
+// Styled/Original
+document.querySelector("#styled").addEventListener("click", function() {
+    video.classList.toggle('oldSchool');
+});
 
